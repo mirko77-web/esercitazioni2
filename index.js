94e1b69d6b6783372469dd8b116c1b702eb2e85a
mirko prisciano <mirkoprisciano@gmail.com>
2025-03-21 16:44:09 +0100

    esercizi

---
+++ index.js
@@ -0,0 +1,54 @@
+/*const cars = ["bmw","audi","jeep"];
+cars[1] = "mercedes"
+console.log(cars)
+
+ const giochi = ["pallone","volley","basket"];
+ giochi[0] = "cavallo";
+ console.log(giochi)
+
+ function myfunction (p1,p2){
+    return p1 * p2 ;
+ }
+ let result= myfunction(5,2);
+ console.log (result);
+
+ const person = {
+    firstName: "John",
+    lastName : "Doe",
+    id     :  5566
+  };
+ console.log 
+("The last name is " + person.lastName);
+let text = "ciao sono mirko"
+console.log(text.length)
+
+let x = "mario"
+let y = "franco"
+console.log (x===y)
+
+ //onvertire in maiuscolo con touppercase//
+ function myFunction() {
+    let text = document.getElementById("demo").innerHTML;
+    document.getElementById("demo").innerHTML =
+    text.toUpperCase();
+  }
+
+    //ciclo for//
+ let data = ["milano","lecce","torino","roma"]
+ for (i=0; i<data.length ; i++){
+    console.log(data[i])
+ }
+
+    //trovare il primo numero maggiore di 18 - metodo find//
+const numbers = [4, 9, 16, 25, 29];
+let first = numbers.find(myFunction);
+
+function myFunction(value, index, array) {
+return value > 18;
+  }
+console.log(first)
+
+//metodo per ordinare in ordine alfabetico//
+const fruits = [ "banana", "mango", "arancia", "mela"];
+fruits.sort();
+console.log (fruits);*/
